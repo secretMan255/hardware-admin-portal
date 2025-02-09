3bfcf28fa8c3a999ab60c2602d46d1b6d4253f3e
yiliang <yapyiliangww321@gmail.com>
2025-02-09 18:09:33 +0800

    add router

---
+++ src/app/ui/post.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+
+type Post = {
+     slug: string
+     title: string
+}
+
+async function getPosts(): Promise<Post[]> {
+     return [
+          { slug: 'home-post', title: 'Home' },
+          { slug: 'product-post', title: 'Product' },
+          { slug: 'signin-post', title: 'Signin' },
+     ]
+}
+
+export default async function Post() {
+     const posts = await getPosts()
+
+     return (
+          <ul>
+               {posts.map((post: any) => (
+                    <li key={post.slug}>
+                         <Link href={`/${post.slug}`}>{post.title}</Link>
+                    </li>
+               ))}
+          </ul>
+     )
+}
