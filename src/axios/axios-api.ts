b6559d99aa8f39855dd3d6f09758a8ea438f5b20
yiliang <yapyiliangww321@gmail.com>
2025-02-10 21:16:34 +0800

    implement axios

---
+++ src/axios/axios-api.ts
@@ -0,0 +1,32 @@
+import axios, { type AxiosInstance, type AxiosResponse, type InternalAxiosRequestConfig } from 'axios'
+
+export class AxiosClient {
+     private static instance: AxiosInstance
+
+     private constructor() {}
+
+     public static getInstance(): AxiosInstance {
+          if (!this.instance) {
+               this.instance = axios.create({
+                    baseURL: process.env.API_BASE_URL,
+                    timeout: 10000,
+                    headers: {
+                         'Content-Type': 'application/json',
+                         Authorization: `Bearer ${process.env.BEREAR_TOKEN}`,
+                    },
+                    withCredentials: true,
+               })
+
+               this.instance.interceptors.request.use(
+                    (config: InternalAxiosRequestConfig) => config,
+                    (error) => Promise.reject(error)
+               )
+
+               this.instance.interceptors.response.use(
+                    (response: AxiosResponse) => response.data,
+                    (error) => Promise.reject(error)
+               )
+          }
+          return this.instance
+     }
+}
