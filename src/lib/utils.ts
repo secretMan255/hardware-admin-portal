4a5d9b1e037b0db9da5bf6eaa165de02bf142799
yiliang <yapyiliangww321@gmail.com>
2025-02-13 16:55:15 +0800

    add convert utc to local and status functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+import * as moment from 'moment'
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+     return twMerge(clsx(inputs))
+}
+
+enum Status {
+     Deactive = 0,
+     Active = 1,
+}
+
+export function convertStatus(status: number): string {
+     return Status[status] ?? 'Unknown'
+}
+
+export function convertUtcToLocal(utcDatetime: string) {
+     return moment.utc(utcDatetime).local().format('YYYY-MM-DD')
 }
