38074369d8000d73181879d6562df456deefb298
yiliang <yapyiliangww321@gmail.com>
2025-02-20 14:54:23 +0800

    add more router protection

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,9 +6,6 @@ export async function middleware(request: NextRequest) {
      const authToken = (await cookies()).get('authToken')?.value
      const pathname = request.nextUrl.pathname
 
-     console.log('Request Cookies:', request.headers.get('cookie'))
-     console.log('authToken: ', authToken, 'pathname: ', pathname)
-
      // Allow static files, API requests, and public assets
      if (
           pathname.startsWith('/_next/') || // Next.js static files
@@ -20,7 +17,7 @@ export async function middleware(request: NextRequest) {
      }
 
      // Routes that require authentication
-     const protectedRoutes = ['/product', '/item', '/']
+     const protectedRoutes = ['/product', '/item', '/', '/mainproduct', '/image', '/carousel']
      if (!authToken && protectedRoutes.some((route) => pathname.startsWith(route))) {
           // If not authenticated and trying to access a protected route, redirect to /login
           if (pathname !== '/login') {
@@ -29,7 +26,7 @@ export async function middleware(request: NextRequest) {
           }
      }
 
-     if (authToken && pathname === '/signin') {
+     if (authToken && pathname === '/login') {
           // If authenticated and trying to access /signin, redirect to a protected route
           const homeUrl = new URL('/product', request.url)
           return NextResponse.redirect(homeUrl)
